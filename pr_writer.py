eff6395956c2493d5e44f4004056c56c7d41d5ab
Max Bograd <Mbograd@PM-G9G06DK6TN.local>
2025-06-12 10:48:31 -0700

    commented the layout for pr_writer

---
+++ pr_writer.py
@@ -0,0 +1,9 @@
+#read commits
+#read PR template (docs/pull_request_template.md)
+#draft PR with comments based on commit changes
+#create PR using GitHub api
+
+#second option once ready for review
+#read PR template
+#draft PR with comments based on new commit changes
+#update PR and convert to regular (not draft)
